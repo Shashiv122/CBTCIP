c4d33f27e9739dfe5d57c7a8ac89699f61e07f6c
Shashiv122 <139039242+Shashiv122@users.noreply.github.com>
2024-06-30 12:02:37 +0530

    Update Mastermind_Game.py

--- Mastermind_Game.py
+++ Mastermind_Game.py
@@ -1,2 +1,13 @@
-Import Random
+Import random  
 
+
+def judgement(confidential, guess )
+    con_list = list(confidential)
+    g_list = list(guess)
+		Correct_dig = sum(c == g for c, g in zip(confidential, guess)):
+		con_count=count(con_list)
+		gue_count=count(g_list)
+		m=min(con_count,gue_count)
+		avail_dig = sum(  for m in g_list)
+		return correct_dig, avail_dig-correct_dig
+										
