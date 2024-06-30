1630e6e0cc943b5d622eb42328fd686afe89d3ce
Shashiv122 <139039242+Shashiv122@users.noreply.github.com>
2024-06-30 21:20:54 +0530

    Update Rock_Paper_Scissor_Game.py

--- Rock_Paper_Scissor_Game.py
+++ Rock_Paper_Scissor_Game.py
@@ -1,4 +1,13 @@
 #Rock Paper Scissor Game
 def comapriison( p1, c):
-        if p1==Rock and c==Scissor:
+        scorep1=0
+        scorec=0
+        if p1=="Rock" and c=="Scissor":
                 print("You win!",\n,"Rock crushes Scissor")
+                scorep1+=1
+        elif p1=="Scissor" and c=="Paper":
+                print("You win!",\n,"Scissor cuts Paper")
+                scorep1+=1
+        elif p1=="Paper" and c=="Rock"
+                print("You win!",\n,"Scissor cuts Paper")
+                scorep1+=1
