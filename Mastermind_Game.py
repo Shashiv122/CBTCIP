83c6dbf492f96f3d63ed1321fc1791125f9dffb3
Shashiv122 <139039242+Shashiv122@users.noreply.github.com>
2024-06-30 19:16:05 +0530

    Update Mastermind_Game.py

--- Mastermind_Game.py
+++ Mastermind_Game.py
@@ -1,34 +1,53 @@
-Import random  
+def judgement(confidential, guess ):
+        con_list = list(confidential)
+        g_list = list(guess)
+        correct_dig = sum(c == g for c, g in zip(confidential, guess))
+        con_count=len(con_list)
+        gue_count=len(g_list)
+        m=min(con_count,gue_count)
+        avail_dig = sum(min(con_list.count(d), g_list.count(d)) for d in set(g_list))
+        return correct_dig, avail_dig-correct_dig
 
-
-def judgement(confidential, guess )
-    con_list = list(confidential)
-    g_list = list(guess)
-		Correct_dig = sum(c == g for c, g in zip(confidential, guess)):
-		con_count=count(con_list)
-		gue_count=count(g_list)
-		m=min(con_count,gue_count)
-		avail_dig = sum(  for m in g_list)
-		return correct_dig, avail_dig-correct_dig
-										
 
 def Mastermind_Game():
-	print("Welcome to the world of Mastermind")
+        print("Welcome to the world of Mastermind")
 
-	#player1 set the confidential number
-	p1_con = int( input( " Player 1: Enter your confidential number: "))
-	p2_tries = 0
-	correct = False
+        #player1 set the confidential number
+        p1_con = input( " Player 1: Enter your confidential number: ")
+        p2_tries = 0
+        correct = False
 
 
-	#Player2 guesses the confidential number
-	while not correct:
-		guess = int(input("Player 2: Guess the number: "))
-		p2_tries +=1
-		correct_dig , misplaced_dig = judgement(p1_con, guess)
+        #Player2 guesses the confidential number
+        while not correct:
+                guess = input("Player 2: Guess the number: ")
+                p2_tries +=1
+                correct_dig , misplaced_dig = judgement(p1_con, guess)
 
-		if correct_dig == len(p1_con):
-			correct = True
-			print(f"Player 2 guessed correctly in ", p2_tries ," attemopts!")
-		else:
-			print(f"Digit in correct place: ",cooerct_dig ,"\n Misplaced digit: ", misplaced_dig )
+                if correct_dig == len(p1_con):
+                        correct = True
+                        print(f"Player 2 guessed correctly in ", p2_tries ," attemopts!")
+                else:
+                        print(f"Digit in correct place: ",correct_dig ,"Misplaced digit: ", misplaced_dig )
+
+
+
+	#player2 set the confidential number
+        p2_con = input( " Player 2: Enter your confidential number: ")
+        p1_tries = 0
+        correct = False
+
+
+        #Player1 guesses the confidential number
+        while not correct:
+                guess = input("Player 1: Guess the number: ")
+                p1_tries +=1
+                correct_dig , misplaced_dig = judgement(p2_con, guess)
+
+                if correct_dig == len(p1_con):
+                        correct = True
+                        print(f"Player 1 guessed correctly in ", p1_tries ," attemopts!")
+                else:
+                        print(f"Digit in correct place: ",correct_dig ,"Misplaced digit: ", misplaced_dig )
+
+Mastermind_Game()
