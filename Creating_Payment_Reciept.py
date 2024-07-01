5a96f61b0a8f8112890ab0694d653501d8858251
Shashiv122 <139039242+Shashiv122@users.noreply.github.com>
2024-07-01 18:56:43 +0530

    Update Creating_Payment_Reciept.py

--- Creating_Payment_Reciept.py
+++ Creating_Payment_Reciept.py
@@ -1,2 +1,26 @@
 import os
+pip install fpdf
+import fpdf from FPDF
+import datetime
 
+#creating class named recieptpdf for the details regarding the transaction and storing the transaction details
+#initialization of the class  recieptpdf
+class reciept_pdf(FPDF):
+
+        #declaring the function that helps to crate the payment reciept
+        #declaring the header function
+        def head(s):
+                s.set_foont("Eras Demi ITC", "B",12)
+                s.cell(0,12,"----* XYZ Shop *----",1,1,"c")
+
+
+        #declaring the footer function of payment reciept
+        def foot(s):
+                s.set_y(-15)
+                s.set_font("Cambria","I",7)
+                s.cell(0,12,f'page ',s.page_no(),0,0,"c")
+
+
+        #declaring the title function of the payment reciept
+
+        
