20f344cc64c1585e2f3ad5a416e90c00ad3c6467
Shashiv122 <139039242+Shashiv122@users.noreply.github.com>
2024-07-02 11:59:06 +0530

    Update Creating_Payment_Reciept.py

--- Creating_Payment_Reciept.py
+++ Creating_Payment_Reciept.py
@@ -9,56 +9,56 @@ class reciept_pdf(FPDF):
 
         #declaring the function that helps to crate the payment reciept
         #declaring the header function
-        def head(s):
-                s.set_foont("Eras Demi ITC", "B",12)
-                s.cell(0,12,"----* XYZ Shop *----",0,1,"c")
+        def head(self):
+                self.set_foont("Arial", "B",12)
+                self.cell(0,12,"----* XYZ Shop *----",0,1,"c")
 
 
         #declaring the footer function of payment reciept
-        def foot(s):
-                s.set_y(-15)
-                s.set_font("Cambria","I",7)
-                s.cell(0,12,f'page {s.page_no()}',0,0,"c")
+        def foot(self):
+                self.set_y(-15)
+                self.set_font("Arial","I",7)
+                self.cell(0,12,f'page {s.page_no()}',0,0,"c")
 
 
         #declaring the title function of the payment reciept
-        def heading(s,h1):
-                s.set_font("Arial", "B",18)
-                s.cell(0,12,h1,0,1,"c")
+        def heading(self,h1):
+                self.set_font("Arial", "B",18)
+                self.cell(0,12,h1,0,1,"c")
 
 
         #declaring transaction details function of the paayment details 
-        def transact_detail(s,transaction_id, cust_name, date):
-                s.set_font("Arial","",13)
-                s.cell(0,10,f"TRANSACTION ID:  {transaction_id}",0,1)
-                s.cell(0,10,f"CUSTOMER NAME: {cust_name}",0,1)
-                s.cell(0,10,f"DATE: {date}",0,1)
+        def transact_detail(self,transaction_id, cust_name, date):
+                self.set_font("Arial","",13)
+                self.cell(0,10,f"TRANSACTION ID:  {transaction_id}",0,1)
+                self.cell(0,10,f"CUSTOMER NAME: {cust_name}",0,1)
+                self.cell(0,10,f"DATE: {date}",0,1)
 
 
         #declaring the item list function for payment reciept
-        def itemslist(s,item):
-                s.set_font("Calibri (Body)","B",11)
-                s.cell(35,5,"Sr No.",1)
-                s.cell(35,5,"Item",1)
-                s.cell(35,5,"Quantity",1)
-                s.cell(35,5,"Unit Price",1)
-                s.cell(35,5,"Total Price",1)
-                s.ln()
-                s.set_font("Arial","",11)
+        def itemslist(self,item):
+                self.set_font("Arial","B",11)
+                self.cell(35,5,"Sr No.",1)
+                self.cell(35,5,"Item",1)
+                self.cell(35,5,"Quantity",1)
+                self.cell(35,5,"Unit Price",1)
+                self.cell(35,5,"Total Price",1)
+                self.ln()
+                self.set_font("Arial","",11)
                 for i, (Itm, Qty, Unit_P) in enumerate(items,1):
-                        s.cell(35,5,str(i),1)
-                        s.cell(35,5,Itm,1)
-                        s.cell(35,5,str(Qty),1)
-                        s.cell(35,5,f'${Unit_P:.2f}',1)
-                        s.cell(35,5,f'${Qty*Unit_P:.2f}',1) 
-                        s.ln()
+                        self.cell(35,5,str(i),1)
+                        self.cell(35,5,Itm,1)
+                        self.cell(35,5,str(Qty),1)
+                        self.cell(35,5,f'${Unit_P:.2f}',1)
+                        self.cell(35,5,f'${Qty*Unit_P:.2f}',1) 
+                        self.ln()
 
 
         #Declaring the funcion to sum up the total amount of payment Reciept
         def total(s, t):
-                s.set_font("Calibri (Body)","B",11)
-                s.cell(175,5,"Total Amount",1)
-                s.cell(35,5,f"${t:.2f}",1)
+                self.set_font("Calibri (Body)","B",11)
+                self.cell(175,5,"Total Amount",1)
+                self.cell(35,5,f"${t:.2f}",1)
 
  #Declaring the function for generating the payment reciept 
 def gen_receipt(transaction_id,cust_name, items):
